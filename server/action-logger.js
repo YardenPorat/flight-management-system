8adb470b44f34a6178df48150b04077a84ac8650
Yarden Porat <yardenporat@gmail.com>
2021-09-26 13:08:53 +0300

    handle dev

--- server/action-logger.js
+++ server/action-logger.js
@@ -1,7 +1,7 @@
 const { ClientAction } = require('./models/action-model');
 
 async function logAction(action, data) {
-    const log = new ClientAction({ action, data, env: process.env.NODE_ENV });
+    const log = new ClientAction({ action, data, env: process.env.NODE_ENV ?? 'dev' });
     await log.save();
 }
 
